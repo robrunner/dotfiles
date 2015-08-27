1de0c04c00e2a117d71f512d7ef9edd9b2963e78
Robert Nachlinger <robert.nachlinger@googlemail.com>
2015-08-27 13:54:28 +0200

    removed double shell call with variable

--- clementine-status.sh
+++ clementine-status.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-if [ $(qdbus org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus) == "Stopped" ]
+status=$(qdbus org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus)
+if [ $status == "Stopped" ]
 then
     out="■"
 else
     out=$(qdbus org.mpris.clementine /Player org.freedesktop.MediaPlayer.GetMetadata | grep title: | sed 's/.*: //')
-        if [ $(qdbus org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus) == "Playing" ]
+        if [ $status == "Playing" ]
         then
             out="▶ $out"
         else
