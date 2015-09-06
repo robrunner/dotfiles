8c9b9452e976385e731bbae93c38b3802a81a1d2
Robert Nachlinger <robert.nachlinger@gmail.com>
2015-09-06 12:07:46 +0200

    symlink all config files to /etc/

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc tmux.conf"    # list of files/folders to symlink in homedir
+files="bashrc vimrc tmux.conf tmux.color.conf"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -27,7 +27,5 @@ for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    sudo ln -s $dir/$file /etc/$file
 done
-
-sudo ln -s $dir/tmux.color.conf /etc/tmux.color.conf
