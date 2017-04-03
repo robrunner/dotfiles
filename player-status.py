f1080d5fb90e7136b2ae45dcbed764fd252f7bdc
Robert Nachlinger <robert.nachlinger@gmail.com>
2017-04-03 18:17:56 +0200

    continued working

--- player-status.py
+++ player-status.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 import os
 import subprocess
+
+ping = subprocess.run(["ping", "-c", "1", "-W", "1", "kodi"]).returncode
+
+if ping == 0:
+    kodi = 1;
+else:
+    kodi = 0;
+
+status = subprocess.run(["curl", "-s", "--user", "kodi:Ni3P5%Wp", "--header", "Content-Type: application/json", "--data-binary", "{ \"id\": 1, \"jsonrpc\": \"2.0\", \"method\": \"Player.GetActivePlayers\", \"params\": {} }", "kodi:8080/jsonrpc"], stdout = subprocess.PIPE).stdout;
+
+print(status);
